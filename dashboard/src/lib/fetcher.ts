e43b09cbf0c5f6badc996de6693ac81146fd92bf
hidi <alstjq1012@gmail.com>
2026-04-09 00:20:29 +0900

    wip

---
+++ dashboard/src/lib/fetcher.ts
@@ -0,0 +1,8 @@
+export async function fetcher(url: string) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const data = await res.json().catch(() => ({}));
+    throw new Error(data.error || `Request failed (${res.status})`);
+  }
+  return res.json();
+}
